97f1ed0108a7d6dede52abfabeceb301658577cc
Kyle Poole <poolekyle@gmail.com>
2020-03-29 12:52:28 -0700

    Take argument for different page support, offline mode, etc.

--- parser.py
+++ parser.py
@@ -6,6 +6,102 @@ import random
 import re
 import json
 import os
+import sys
+
+def get_html_facts(html):
+  # On the music summary pages this will grab the "events"
+  # that happened for a given year.
+  elements = []
+  element = html.select("h2>span[id='Events']")[0].parent
+  
+  # Only rip data between the two <h2>s
+  cut_off_element = element.find_next("h2")
+
+  while element != cut_off_element:
+    if hasattr(element, "select") == False:
+      element = element.next_element
+      continue
+    if element.select("ul li") and not element.select("ul li ul"):
+      elements.extend(element.select("ul li"))
+    element = element.next_element
+
+  # Ensure that we have more than one anchor element - anything with less is
+  # unlikely to be a quizzable fact.
+  return list(filter(lambda x: len(x.select("a")), elements))
+
+def fetch_and_parse_html(topic):
+  # TODO: We should save the output of the HTML as well to ensure it syncs up
+  # with fact_links and fulltext_metadata.
+  print("### " + topic + " does not exist - fetching data")
+  html = urllib.request.urlopen(wikipedia.page(topic).url).read().decode("utf-8")
+  parsed_html = BeautifulSoup(html, "html.parser")
+  
+  html_file = open(topic + "_page.html", "w")
+  html_file.truncate(0)
+  html_file.write(html)
+  html_file.close()
+
+  html_facts = get_html_facts(parsed_html)
+
+  # TODO: Does this need to go?
+  fact_links = {
+    "location": [],
+    "person": [],
+    "band": [],
+    "song": [],
+    "album": [],
+    "genre": [],
+    "event": [],
+    "date": [],
+    "award": [],
+    "uncategorized": []
+  }
+  fulltext_metadata = []
+
+  for base_index, facts in enumerate(html_facts):
+    current_set = {}
+    for question_index, current_fact in enumerate(facts):
+      # FIXME: Is there a better way to duck type this?
+      if hasattr(current_fact, "select"):
+        # TODO: Should find a way to categorize here.
+        # TODO: Exclude dates.
+        # TODO: Exclude citations
+        # FIXME: This is very slow - should be a way to do this more concurrently.
+        try:
+          # FIXME: This is sloppy.
+          stripped_url = re.sub(r".+/wiki/", "", current_fact["href"]).replace("/wiki/", "").replace("(", " ").replace(")", " ").replace("_", " ").replace("  ", " ").casefold()
+          summary = wikipedia.summary(stripped_url, sentences = 2)
+          category = categorize(summary)
+          current_set[question_index] = {
+            "fact": current_fact.text,
+            "summary": summary,
+            "category": category 
+          }
+          fact_links[category].append((base_index, question_index))
+        except Exception as e:
+          print("There was an error with retrieving from wikipedia for " + current_fact.text, " aka " + stripped_url)
+          print(e)
+
+    fulltext_metadata.append(current_set)
+
+  print("### HTML FACTS")
+  print(html_facts)
+
+  fact_links_file = open(topic + "_fact_links.json", "w")
+  fact_links_file.truncate(0)
+  fact_links_file.write(json.dumps(fact_links))
+  fact_links_file.close()
+
+  fulltext_metadata_file = open(topic + "_fulltext_metadata.json", "w")
+  fulltext_metadata_file.truncate(0)
+  fulltext_metadata_file.write(json.dumps(fulltext_metadata))
+  fulltext_metadata_file.close()
+
+  return {
+    "html_facts": html_facts,
+    "fact_links": fact_links,
+    "fulltext_metadata": fulltext_metadata,
+  }
 
 def categorize(summary):
   categories = {
@@ -85,94 +181,46 @@ def categorize(summary):
         return category
   return "uncategorized"
 
-def main():
-  # TODO: We should save the output of the HTML as well to ensure it syncs up
-  # with fact_links and fulltext_metadata.
-  html = urllib.request.urlopen(wikipedia.page("2000 in music").url).read()
-  parsed_html = BeautifulSoup(html, "html.parser")
-  
-  # On the music summary pages this will grab the "events"
-  # that happened for a given year.
-  elements = []
-  element = parsed_html.select("h2>span[id='Events']")[0].parent
-  
-  # Only rip data between the two <h2>s
-  cut_off_element = element.find_next("h2")
+def files_exist_and_have_data(topic):
+  if (os.path.exists(topic + "_page.html") == False or 
+      os.path.exists(topic + "_fact_links.json") == False or
+      os.path.exists(topic + "_fulltext_metadata.json") == False):
+        return False
+  if (os.stat(topic + "_page.html").st_size == 0 and
+      os.stat(topic + "_fact_links.json").st_size == 0 and
+      os.stat(topic + "_fulltext_metadata.json").st_size == 0):
+        return False
 
-  # FIXME: This might be able to be done with a map
-  while element != cut_off_element:
-    if hasattr(element, "select") == False:
-      element = element.next_element
-      continue
-    if element.select("ul li") and not element.select("ul li ul"):
-      elements.extend(element.select("ul li"))
-    element = element.next_element
+  return True
 
-  # Ensure that we have more than one anchor element - anything with less is
-  # unlikely to be a quizzable fact.
-  music_facts = list(filter(lambda x: len(x.select("a")), elements))
-
+def main(topic):
   # Grab the links from the facts and attempt to categorize them.
   # Need a data shape that will let us know where in the fact list the link belongs to.
-  if os.stat("fact_links.json").st_size != 0 and os.stat("fulltext_metadata.json").st_size != 0:
-    fact_links = json.loads(open("fact_links.json", "r").read())
-    fulltext_metadata = json.loads(open("fulltext_metadata.json", "r").read())
+  if files_exist_and_have_data(topic):
+    html = BeautifulSoup(open(topic + "_page.html", "r").read(), "html.parser")
+    html_facts = get_html_facts(html)
+    fact_links = json.loads(open(topic + "_fact_links.json", "r").read())
+    fulltext_metadata = json.loads(open(topic + "_fulltext_metadata.json", "r").read())
   else:
-    fact_links = {
-      "location": [],
-      "person": [],
-      "band": [],
-      "song": [],
-      "album": [],
-      "genre": [],
-      "event": [],
-      "date": [],
-      "award": []
-    }
-    fulltext_metadata = []
+    new_data = fetch_and_parse_html(topic)
+    html_facts = get_html_facts(new_data["html_facts"]) 
+    fact_links = new_data["fact_links"]
+    fulltext_metadata = new_data["fulltext_metadata"]
 
-    for base_index, facts in enumerate(music_facts):
-      print(str(base_index) + " " + str(facts))
-      current_set = {}
-      for question_index, current_fact in enumerate(facts):
-        # FIXME: Is there a better way to duck type this?
-        if hasattr(current_fact, "select"):
-          # TODO: Should find a way to categorize here.
-          # TODO: Exclude dates.
-          # TODO: Exclude citations
-          # FIXME: This is very slow - should be a way to do this more concurrently.
-          try:
-            # FIXME: This is sloppy.
-            stripped_url = re.sub(r".+/wiki/", "", current_fact["href"]).replace("/wiki/", "").replace("(", " ").replace(")", " ").replace("_", " ").replace("  ", " ").casefold()
-            summary = wikipedia.summary(stripped_url, sentences = 2)
-            category = categorize(summary)
-            current_set[question_index] = {
-              "fact": current_fact.text,
-              "summary": summary,
-              "category": category 
-            }
-            fact_links[category].append((base_index, question_index))
-          except:
-            print("There was an error with retrieving from wikipedia for " + current_fact.text, " aka " + stripped_url)
+  # Substitute a random link in the writeup for empty spaces.
+  question_number = random.randint(0, len(html_facts)-1)
+  question = html_facts[question_number]
 
-      fulltext_metadata.append(current_set)
 
-    fact_links_file = open("fact_links.json", "w")
-    fact_links_file.truncate(0)
-    fact_links_file.write(json.dumps(fact_links))
-    fact_links_file.close()
+  # Ensure that the answer exists in the questions - when the lookup fails
+  # there's the possibility of a link not containing a fact
+  is_valid_fact = False
+  while not is_valid_fact:
+    answer_fact_number = random.randint(0, len(question.select("a"))-1)
+    print(fulltext_metadata[question_number])
+    if str(answer_fact_number*2) in fulltext_metadata[question_number]:
+      is_valid_fact = True
 
-    fulltext_metadata_file = open("fulltext_metadata.json", "w")
-    fulltext_metadata_file.truncate(0)
-    fulltext_metadata_file.write(json.dumps(fulltext_metadata))
-    fulltext_metadata_file.close()
-
-  # Substitute a random2000 link in the writeup for empty spaces.
-  question_number = random.randint(0, len(music_facts)-1)
-  question = music_facts[question_number]
-
-  # random_fact = random.choice(music_facts)
-  answer_fact_number = random.randint(0, len(question.select("a"))-1)
   answer_fact_category = fulltext_metadata[question_number][str(answer_fact_number*2)]["category"]
   false_answers = []
   for _ in range(4):
@@ -185,4 +233,4 @@ def main():
   print("ANSWER IS " + str(fulltext_metadata[question_number][str(answer_fact_number*2)]))
   print("### FALSE ANSWERS: " + str(false_answers))
 
-main()
+main(sys.argv[1].replace(" ", "_"))
